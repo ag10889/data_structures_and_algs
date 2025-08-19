e1020f8a634d1a9519366d010de40cb95de8daf0
Alex George <alexander.george03@icloud.com>
2025-08-19 13:35:37 -0400

    Create Main.java

---
+++ Rectangle/src/main/java/Main.java
@@ -0,0 +1,17 @@
+package main.java;
+import java.util.Scanner;
+
+public class Main extends Rectangle {
+    public static void main(String[] args) {
+        Rectangle rect = new Rectangle();
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter width: ");
+        double x = scanner.nextDouble();
+        System.out.print("Enter height: ");
+        double y = scanner.nextDouble();
+        scanner.close();
+        System.out.println(rect.printRectangle("DefaultRectangle"));
+        Rectangle yourRectangle = new Rectangle(x, y);
+        System.out.println(yourRectangle.printRectangle("YourRectangle"));
+    }
+}
