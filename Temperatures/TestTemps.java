7c313121b0a0842c8185fdb833dd7981e25d393b
Alex George <alexander.george03@icloud.com>
2025-08-19 15:26:27 -0400

    my mistake, wrong file ext

--- Temperatures/TestTemps.java
+++ Temperatures/TestTemps.java
@@ -10,7 +10,7 @@ package Temperatures;
 public class TestTemps extends DailyTemps {
 
     public static void main(String[] args) {
-        double[] testData = {30.0, 79.2, 99.9, 63.5, 57.8, 29.7, 37.9}
+        double[] testData = {30.0, 79.2, 99.9, 63.5, 57.8, 29.7, 37.9};
         DailyTemps dailyTemps = new DailyTemps();
         DailyTemps dailyTempsTwo = new DailyTemps(testData);
 
