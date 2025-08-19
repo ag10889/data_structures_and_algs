6ab787bb8f4903b155b9d92ab4ecd547beda820e
Alex George <alexander.george03@icloud.com>
2025-08-19 14:07:30 -0400

    reworks

--- Temperatures/DailyTemps.java
+++ Temperatures/DailyTemps.java
@@ -1,3 +1,38 @@
+package Temperatures;
+/*
+ * Name : Alex George
+ * Class: CS 3305/SectionW04
+ * Term: Fall 2025
+ * Instructor: Prof. Wang
+ * Assignment: 1
+ * IDE Name: IntelliJ IDEA
+ */
+
+import java.util.ArrayList;
+
 public class DailyTemps {
+    private ArrayList<String> dailyTemps = new ArrayList<>(7);
+    /*
+     * Monday = temps[0]
+     * Tuesday = temps[1]
+     * etc
+     */
+    public DailyTemps() {
+        // Initialize the daily temperatures with default values
+        dailyTemps.add("Monday: 0.0");
+        dailyTemps.add("Tuesday: 0.0");
+        dailyTemps.add("Wednesday: 0.0");
+        dailyTemps.add("Thursday: 0.0");
+        dailyTemps.add("Friday: 0.0");
+        dailyTemps.add("Saturday: 0.0");
+        dailyTemps.add("Sunday: 0.0");
+    }
+
+    
+
+
+    
+
+     
 
 }
