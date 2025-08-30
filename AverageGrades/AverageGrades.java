8050c720d3d82575d4a26d25af4d1d6bfb276416
Alex George <alexander.george03@icloud.com>
2025-08-30 13:31:24 -0400

    Update AverageGrades.java

--- AverageGrades/AverageGrades.java
+++ AverageGrades/AverageGrades.java
@@ -12,14 +12,17 @@ import java.util.Scanner;
  /* TODO
   *  -[x] create public static to have a menu bar with selection choices
   * -[X] create choices read class size (some sort of int)
-  * -[] read class grades (maybe some sort of a array)
-  * -[] compute class average
-  * -[] exit
+  * -[X] read class grades (maybe some sort of a array)
+  * -[X] compute class average
+  * -[X] exit
   */
 public class AverageGrades {
+
     public static void main(String[] args) {
+        
         int classSize = 0;
         int[] grades = null;
+        
         while(true){
             System.out.println("Main Menu");
             System.out.println("1. Read class size");
@@ -33,13 +36,24 @@ public class AverageGrades {
             
             switch (option) {
                 case 1:
-                    // TODO: Implement read class size
+                    // TODO: read a class size
+                    System.out.println("Enter class size:");
+                    classSize = scanner.nextInt();
+                    grades = new int[classSize];
                     break;
                 case 2:
-                    // TODO: Implement read class grades
+                    // TODO: read class grades
+                    System.out.println("Enter class grades:");
+                    for (int i = 0; i < classSize; i++) {
+                        grades[i] = scanner.nextInt();
+                    }
                     break;
                 case 3:
-                    // TODO: Implement compute class average
+                    // TODO: compute class average
+                    double average = findAverage(grades, classSize);
+                    if (average != -1) {
+                        System.out.println("Class average: " + average);
+                    }
                     break;
                 case 4:
                     System.out.println("Exiting program.");
@@ -48,17 +62,17 @@ public class AverageGrades {
             }
         }
     }
-    public static double findAveragee(int[] grades, int classSize) {
-    if (grades == null || grades.length == 0) {
-        System.out.println("No grades available to compute average.");
-        return -1;  // Meaningful value indicating no grades present
-    }
-    double sum = grades[0];
-    if (classSize == 1) {
-        return sum; // Base case: only one grade
-    } else {
-        return sum + findAveragee(java.util.Arrays.copyOfRange(grades, 1, grades.length), classSize - 1);
-    }
-}
+    public static double findAverage(int[] grades, int classSize) {
+        if (grades == null || grades.length == 0) {
+            System.out.println("No grades available to compute average.");
+            return -1;  // Meaningful value indicating no grades present
+            }
+            double sum = grades[0];
+            if (classSize == 1) {
+                return sum; // Base case: only one grade
+            } else {
+                return sum + findAverage(java.util.Arrays.copyOfRange(grades, 1, grades.length), classSize - 1);
+            }
+        }
 
 }
